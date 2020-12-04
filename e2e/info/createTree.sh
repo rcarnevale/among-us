21a57c466fa3c057db8f77a722fa1e5ca78c9053
rodrigo <carnevale.lemobs@gmail.com>
2020-12-04 14:14:02 -0300

    commit inicial

---
+++ e2e/info/createTree.sh
@@ -0,0 +1,19 @@
+# Create estrutura
+
+# CREATE PATH #
+    # mkdir ./src/app/modules
+    # mkdir ./src/app/shared
+    # mkdir ./src/app/design-system
+
+# CREATE COMPONENTS #
+# home
+    # mkdir -p ./src/app/modules/home/components
+    # mkdir -p ./src/app/modules/home/pages    
+    # mkdir -p ./src/app/modules/home/services/DTO/request
+    # mkdir -p ./src/app/modules/home/services/DTO/response
+    # npx ng g m modules/home/
+    # npx ng g s modules/home/services/home --skipTests=true
+    # npx ng g c modules/home/pages/home
+    # npx ng generate module modules/home/home-routing --flat --module=home
+
+
