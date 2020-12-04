f2f6dfb06c82a1cc4d38695c0df5d9f6f4387b44
rodrigo <carnevale.lemobs@gmail.com>
2020-12-04 17:09:46 -0300

    costruindo o personagem e alguns métodos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './modules/home/pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('./modules/home/home.module').then(m => m.HomeModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ColorsDTO } from '../../services/DTO/colors.DTO';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public selectedColor = '#c51111';
+  public allColors: Array<ColorsDTO> = [
+    { name: 'Red', color: '#c51111' },
+    { name: 'Blue', color: '#132ed1' },
+    { name: 'DarkGreen', color: '#117f2d' },
+    { name: 'Pink', color: '#fb69be' },
+    { name: 'Orange', color: '#e8790e' },
+    { name: 'Yellow', color: '#f5f557' },
+    { name: 'black', color: '#3e474e' },
+    { name: 'White', color: '#d6e1f3' },
+    { name: 'Purple', color: '#6d3ec0' },
+    { name: 'Brown', color: '#764a1d' },
+    { name: 'Cyan', color: '#28eae1' },
+    { name: 'LightGreen', color: '#45d633' },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  changeColor(color: ColorsDTO){
+    this.selectedColor = color.color;
   }
 
 }
